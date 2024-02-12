26120a330136a4856cdda9a6e9f1a1f0edeeedf4
Walter Lucetti <walter.lucetti@stereolabs.com>
2024-02-12 20:00:18 +0100

    Good PID calibration

--- kangaroo_x2_lib/examples/example_mixed_model.cpp
+++ kangaroo_x2_lib/examples/example_mixed_model.cpp
@@ -39,20 +39,21 @@ int main(int argc, char *argv[]) {
   }
   std::cout << "Channel 'T' started" << std::endl;
 
-  // err = drive.units(565, 4583);
-  // if (err != KANGAROO_NO_ERROR) {
-  //   std::cerr << "Error setting drive units: " << toString(err)
-  //             << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // std::cout << "Channel 'D' unit set" << std::endl;
+  err =
+      drive.units(565, 1833);  // 100 CPR, 180 mm wheel radius, 320 mm wheelbase
+  if (err != KANGAROO_NO_ERROR) {
+    std::cerr << "Error setting drive units: " << toString(err) << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << "Channel 'D' unit set" << std::endl;
 
-  // err = turn.units(360, 8147);
-  // if (err != KANGAROO_NO_ERROR) {
-  //   std::cerr << "Error setting turn units: " << toString(err) << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // std::cout << "Channel 'T' unit set" << std::endl;
+  err =
+      turn.units(360, 3256);  // 100 CPR, 180 mm wheel radius, 320 mm wheelbase
+  if (err != KANGAROO_NO_ERROR) {
+    std::cerr << "Error setting turn units: " << toString(err) << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << "Channel 'T' unit set" << std::endl;
 
   long min_d = drive.getMin().value();
   long max_d = drive.getMax().value();
@@ -63,12 +64,23 @@ int main(int argc, char *argv[]) {
   long max_t = turn.getMax().value();
   std::cout << "Turn - Min: " << min_t << " / Max: " << max_t << std::endl;
 
-  drive.setSpeed(0,1000);
-  turn.setSpeed(0,360);
+  double fw_avg = 0.0;
+  double bw_avg = 0.0;
+  double cw_avg = 0.0;
+  double ccw_avg = 0.0;
+
+  int d_acc = 2000;
+  int t_acc = 720;
 
   int32_t speed = 2200;
 
-  err = drive.setSpeed(speed, 1000, KANGAROO_MOVE_DEFAULT).status().error();
+  int read_count = 150;
+  uint16_t real_count = 0;
+
+  drive.setSpeed(0, d_acc);
+  turn.setSpeed(0, t_acc);
+
+  err = drive.setSpeed(speed, d_acc, KANGAROO_MOVE_DEFAULT).status().error();
   if (err != KANGAROO_NO_ERROR) {
     std::cerr << "Error setting forward speed: " << toString(err) << std::endl;
     err = drive.powerDown();
@@ -84,18 +96,30 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < read_count; i++) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::cout << "D speed: " << drive.getSpeed().value() << "/" << drive.getSetPointSpeed().value()<< std::endl
+    double d = drive.getSpeed().value();
+    double t = turn.getSpeed().value();
+    if (i >= 100) {
+      real_count++;
+      fw_avg += d;
+    }
+    std::cout << "D speed: " << d << "/" << drive.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
-    std::cout << "T speed: " << turn.getSpeed().value() << "/"
-              << turn.getSetPointSpeed().value() << std::endl
+    std::cout << "T speed: " << t << "/" << turn.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
   }
+
+  fw_avg /= real_count;
+
+  drive.setSpeed(0, d_acc);
+  turn.setSpeed(0, t_acc);
 
   speed = -2200;
 
-  err = drive.setSpeed(speed, 1000, KANGAROO_MOVE_DEFAULT).status().error();
+  err = drive.setSpeed(speed, d_acc, KANGAROO_MOVE_DEFAULT).status().error();
   if (err != KANGAROO_NO_ERROR) {
     std::cerr << "Error setting forward speed: " << toString(err) << std::endl;
     err = drive.powerDown();
@@ -111,22 +135,32 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  for (int i = 0; i < 100; i++) {
+  real_count = 0;
+
+  for (int i = 0; i < read_count; i++) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::cout << "D speed: " << drive.getSpeed().value() << "/"
-              << drive.getSetPointSpeed().value() << std::endl
+    double d = drive.getSpeed().value();
+    double t = turn.getSpeed().value();
+    if (i >= 100) {
+      real_count++;
+      bw_avg += d;
+    }
+    std::cout << "D speed: " << d << "/" << drive.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
-    std::cout << "T speed: " << turn.getSpeed().value() << "/"
-              << turn.getSetPointSpeed().value() << std::endl
+    std::cout << "T speed: " << t << "/" << turn.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
   }
 
-  drive.setSpeed(0, 1000);
-  turn.setSpeed(0, 360);
+  bw_avg /= real_count;
+
+  drive.setSpeed(0, d_acc);
+  turn.setSpeed(0, t_acc);
 
   speed = 720;
 
-  err = turn.setSpeed(speed, 360, KANGAROO_MOVE_DEFAULT).status().error();
+  err = turn.setSpeed(speed, t_acc, KANGAROO_MOVE_DEFAULT).status().error();
   if (err != KANGAROO_NO_ERROR) {
     std::cerr << "Error setting forward speed: " << toString(err) << std::endl;
     err = drive.powerDown();
@@ -142,19 +176,32 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  for (int i = 0; i < 100; i++) {
+  real_count = 0;
+
+  for (int i = 0; i < read_count; i++) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::cout << "D speed: " << drive.getSpeed().value() << "/"
-              << drive.getSetPointSpeed().value() << std::endl
+    double d = drive.getSpeed().value();
+    double t = turn.getSpeed().value();
+    if (i >= 100) {
+      real_count++;
+      ccw_avg += t;
+    }
+    std::cout << "D speed: " << d << "/" << drive.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
-    std::cout << "T speed: " << turn.getSpeed().value() << "/"
-              << turn.getSetPointSpeed().value() << std::endl
+    std::cout << "T speed: " << t << "/" << turn.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
   }
+
+  ccw_avg /= real_count;
+
+  drive.setSpeed(0, d_acc);
+  turn.setSpeed(0, t_acc);
 
   speed = -720;
 
-  err = turn.setSpeed(speed, 360, KANGAROO_MOVE_DEFAULT).status().error();
+  err = turn.setSpeed(speed, t_acc, KANGAROO_MOVE_DEFAULT).status().error();
   if (err != KANGAROO_NO_ERROR) {
     std::cerr << "Error setting forward speed: " << toString(err) << std::endl;
     err = drive.powerDown();
@@ -170,18 +217,37 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  for (int i = 0; i < 100; i++) {
+  real_count = 0;
+
+  for (int i = 0; i < read_count; i++) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::cout << "D speed: " << drive.getSpeed().value() << "/"
-              << drive.getSetPointSpeed().value() << std::endl
+    double d = drive.getSpeed().value();
+    double t = turn.getSpeed().value();
+    if (i >= 100) {
+      real_count++;
+      cw_avg += t;
+    }
+    std::cout << "D speed: " << d << "/" << drive.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
-    std::cout << "T speed: " << turn.getSpeed().value() << "/"
-              << turn.getSetPointSpeed().value() << std::endl
+    std::cout << "T speed: " << t << "/" << turn.getSetPointSpeed().value()
+              << std::endl
               << std::flush;
   }
 
-  drive.setSpeed(0, 1000);
-  turn.setSpeed(0, 360);
+  cw_avg /= real_count;
+
+  drive.setSpeed(0, d_acc);
+  turn.setSpeed(0, t_acc);
+
+  std::cout << std::endl;
+  std::cout << " * Avg forward speed:\t\t" << fw_avg / 1000. << " m/sec"
+            << std::endl;
+  std::cout << " * Avg backward speed:\t\t" << bw_avg / 1000. << " m/sec"
+            << std::endl;
+  std::cout << " * Avg counterclockwise speed:\t" << ccw_avg << " °/sec"
+            << std::endl;
+  std::cout << " * Avg clockwise speed:\t\t" << cw_avg << " °/sec" << std::endl;
 
   return EXIT_SUCCESS;
 }
